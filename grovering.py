7e0a5fc4dd55360170764cf1689d7c32f67f1abc
Simone Perriello <simone.perriello@protonmail.com>
2018-12-23 20:33:47 +0100

    added argparse, various simulators, exports and reorganized code

--- grovering.py
+++ grovering.py
@@ -1,10 +1,4 @@
-from math import log, sqrt, pi
-import numpy as np
-from qiskit import QuantumCircuit, ClassicalRegister, QuantumRegister
 import logging
-import composed_gates
-import pseudoquantumregister as pse
-
 _logger = logging.getLogger(__name__)
 _handler = logging.StreamHandler()
 _formatter = logging.Formatter(
@@ -14,7 +8,6 @@ if (_logger.hasHandlers()):
     _logger.handlers.clear()
 _logger.addHandler(_handler)
 _logger.setLevel(logging.DEBUG)
-draw_circuit = False
 
 
 def initialize_circuit(n):
@@ -304,20 +297,61 @@ def permutation_old_i(circuit, selectors_q, flip_q):
     circuit.barrier()
 
 
-def main():
-    global draw_circuit
-    from os import sys
-    # 1 online simulator, 0 local simulator, 2
-    backend_choice = int(sys.argv[1])
-    # 1 draw, 0 not draw
-    draw = int(sys.argv[2])
-    if (draw == 1):
-        # print("Drawing")
-        draw_circuit = True
-        from qiskit.tools.visualization import circuit_drawer
-        # circuit_drawer(qc, filename='img/grovering.png', output='latex')
-        # circuit_drawer(qc, output="text")
+def usage():
+    import argparse
+    parser = argparse.ArgumentParser(description="Prange isd algorithm")
+    parser.add_argument(
+        'n', metavar='n', type=int, help='the n of the parity matrix H.')
+    parser.add_argument(
+        'r',
+        metavar='r',
+        type=int,
+        help='the r (= n - k) of the parity matrix H.')
+    parser.add_argument(
+        '-r',
+        '--real',
+        action='store_true',
+        help='Invoke the real device (implies -o). Default is simulator.')
+    parser.add_argument(
+        '-o',
+        '--online',
+        action='store_true',
+        help=
+        'Use the online IBMQ devices. Default is local (simulator). This option is automatically set when we want to use a real device (see -r).'
+    )
+    parser.add_argument(
+        '-b',
+        '--backend_name',
+        help=
+        "The name of the backend. At the moment, the available local backends are: qasm_simulator, statevector_simulator and unitary_simulator."
+    )
+    parser.add_argument(
+        '-pd',
+        '--partial_drawing',
+        action='store_true',
+        help='Draw the incremental images of the circuit')
+    parser.add_argument(
+        '-td',
+        '--total_drawing',
+        action='store_true',
+        help='Draw the final image of the circuit')
+    parser.add_argument(
+        '--img_dir',
+        help=
+        'If you want to store the image of the circuit, you need to specify the directory.'
+    )
+    parser.add_argument(
+        '--plot',
+        action='store_true',
+        help='Plot the histogram of the results. Default is false')
+    parser.add_argument(
+        '--export_qasm_file',
+        help='Export the circuit as qasm text in the specified file')
+    args = parser.parse_args()
+    return args
 
+
+def get_sample_matrix_and_random_syndrome(n, r):
     h8 = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 0], [0, 1, 1], [1, 0, 0],
                    [0, 1, 0], [0, 0, 1], [1, 0, 1]]).T
     syndromes7 = np.array([[0, 0, 1], [0, 1, 0], [0, 1, 1], [1, 0, 0],
@@ -329,32 +363,46 @@ def main():
 
     h4 = np.array([[1, 1, 1], [1, 0, 0], [0, 1, 0], [0, 0, 1]]).T
     syndromes4 = np.array([[0, 1, 0], [1, 1, 1], [1, 0, 0], [0, 0, 1]])
+    if (n == 8 and r == 3):
+        h = h8
+        s = syndromes7
+    elif (n == 4 and r == 3):
+        h = h4
+        s = syndromes4
+    else:
+        return None, None
+    return h, s
 
-    h = h8
-    syndromes = syndromes7
 
+def build_circuit(args):
+    n = args.n
+    r = args.r
+    partial_drawing = args.partial_drawing
+    img_dir = args.img_dir
+    _logger.debug("n: {0}, r: {1}, partial_drawing: {2}, img_dir: {3}".format(
+        n, r, partial_drawing, img_dir))
+
+    global h, syndrome
+    h, syndromes = get_sample_matrix_and_random_syndrome(n, r)
     w = 1
-    r = h.shape[0]
-    n = h.shape[1]
-    k = n - r
     _logger.debug("W = {0}; n = {1}; r = {2}".format(w, n, r))
+    # Launch the circuit w/ a random syndrome
     syndrome = syndromes[np.random.randint(syndromes.shape[0])]
-    print("Syndrome is {0}".format(syndrome))
+    _logger.debug("Syndrome is {0}".format(syndrome))
 
     qc, selectors_q = initialize_circuit(n)
     n_choose_w(qc, selectors_q, w)
-    if draw_circuit:
+    if partial_drawing:
         circuit_drawer(
             qc, filename='img/grovering_nchoosew.png', output='latex')
 
     flip_q = permutation(qc, selectors_q, None)
-    if draw_circuit:
+    if partial_drawing:
         circuit_drawer(
             qc, filename='img/grovering_permutation.png', output='latex')
-    # flip_q = permutation_old(qc, selectors_q, None)
     sum_q = matrix2gates(qc, h, selectors_q, None)
     syndrome2gates(qc, sum_q, syndrome)
-    if draw_circuit:
+    if partial_drawing:
         circuit_drawer(
             qc, filename='img/grovering_matrix_syndrome.png', output='latex')
 
@@ -366,18 +414,17 @@ def main():
     pseudo_target_oracle.add_registers(selectors_q, flip_q)
     single_control_sum = oracle(qc, sum_q, pseudo_target_oracle,
                                 pseudo_ancilla_register)
-    if draw_circuit:
+    if partial_drawing:
         circuit_drawer(qc, filename='img/grovering_oracle.png', output='latex')
 
     syndrome2gates(qc, sum_q, syndrome)
     matrix2gates_i(qc, h, selectors_q, sum_q)
-    if draw_circuit:
+    if partial_drawing:
         circuit_drawer(
             qc, filename='img/grovering_matrix_syndrome_i.png', output='latex')
     permutation_i(qc, selectors_q, flip_q)
-    # permutation_old_i(qc, selectors_q, flip_q)
     n_choose_w(qc, selectors_q, w)
-    if draw_circuit:
+    if partial_drawing:
         circuit_drawer(
             qc, filename='img/grovering_permutation_i.png', output='latex')
 
@@ -388,12 +435,14 @@ def main():
     single_control_inversion = inversion_about_zero(
         qc, pseudo_control_inversion, selectors_q[0], pseudo_ancilla_register)
     negate_for_inversion(qc, selectors_q, flip_q)
-    if draw_circuit:
+    if partial_drawing:
         circuit_drawer(
             qc, filename='img/grovering_inversion.png', output='latex')
 
     n_choose_w(qc, selectors_q, w)
     permutation(qc, selectors_q, flip_q)
+
+    # Remaining grover iterations
     rounds = int(round((pi / 2 * sqrt(n) - 1) / 2))
     _logger.debug("{0} rounds required".format(rounds - 1))
     for i in range(rounds - 1):
@@ -415,49 +464,89 @@ def main():
         n_choose_w(qc, selectors_q, w)
         permutation(qc, selectors_q, flip_q)
 
-    # 2 should be statevector
-    if (backend_choice == 0):
-        print("Backend offline")
+    return qc, selectors_q
+
+
+def run(qc, selectors_q, args):
+    real = args.real
+    online = True if real else args.online
+    total_drawing = args.total_drawing
+    img_dir = args.img_dir
+    plot = args.plot
+    backend_name = args.backend_name
+    need_measures = True
+    _logger.debug(
+        "real: {0}, online: {1}, total_drawing: {2}, img_dir: {3}, plot: {4}, backend_name: {5}"
+        .format(real, online, total_drawing, img_dir, plot, backend_name))
+    if (backend_name not in ('statevector_simulator', 'unitary_simulator')):
+        _logger.debug("Adding measures")
+        n = args.n
+        from qiskit import ClassicalRegister
         cr = ClassicalRegister(n, 'cols')
         qc.add_register(cr)
         qc.measure(selectors_q, cr)
-        from qiskit import Aer
-        backend = Aer.get_backend('qasm_simulator')
-    elif (backend_choice == 1):
-        print("Backend online")
-        cr = ClassicalRegister(n, 'cols')
-        qc.add_register(cr)
-        qc.measure(selectors_q, cr)
+    if (online):
+        if (backend_name is None):
+            backend_name = 'ibmq_qasm_simulator'
         from qiskit import IBMQ
         IBMQ.load_accounts()
-        backend = IBMQ.get_backend('ibmq_qasm_simulator')
-    elif (backend_choice == 2):
-        print("Backend statevector")
+        backend = IBMQ.get_backend(backend_name)
+    else:
+        if (backend_name is None):
+            backend_name = 'qasm_simulator'
         from qiskit import Aer
-        backend = Aer.get_backend('statevector_simulator')
+        backend = Aer.get_backend(backend_name)
 
-    print("Preparing execution")
+    _logger.debug("Preparing execution")
     from qiskit import execute
-    print("Execute")
+    _logger.debug("Execute")
     job = execute(qc, backend, shots=4098)
-    print(job.job_id())
+    _logger.debug(job.job_id())
     result = job.result()
-    print("Results ready")
-    if (backend_choice == 0 or backend_choice == 1):
-        counts = result.get_counts(qc)
-        print(counts)
-        print(len(counts))
-        to_plot = counts
-
-    elif (backend_choice == 2):
-        statevector = result.get_statevector(qc)
-        print("State vector is {0}".format(statevector))
-        to_plot = statevector
-
+    _logger.debug("Results ready")
     _logger.debug("Syndrome was {0}; H was \n{1}".format(syndrome, h))
 
-    from qiskit.tools.visualization import plot_histogram
-    plot_histogram(to_plot)
+    if backend_name in 'statevector_simulator':
+        statevector = result.get_statevector(qc)
+        _logger.debug("State vector is\n{0}".format(statevector))
+        if plot:
+            _logger.debug("Plotting")
+            from qiskit.tools.visualization import plot_state_city
+            plot_state_city(statevector)
+    elif backend_name in 'unitary_simulator':
+        unitary = result.get_unitary(qc)
+        _logger.debug("Circuit unitary is:\n{0}".format(unitary))
+    else:  # For qasm and real devices
+        counts = result.get_counts(qc)
+        _logger.debug("{0} results: \n {1}".format(len(counts), counts))
+        if plot:
+            _logger.debug("Plotting")
+            from qiskit.tools.visualization import plot_histogram
+            plot_histogram(counts)
+
+
+def import_fat_modules():
+    global log, sqrt, pi, np, QuantumCircuit, QuantumRegister, composed_gates, pse
+    from math import log, sqrt, pi
+    import numpy as np
+    from qiskit import QuantumCircuit, QuantumRegister
+    import composed_gates
+    import pseudoquantumregister as pse
+
+
+def main():
+    args = usage()
+    import_fat_modules()
+    qc, selectors_q = build_circuit(args)
+    s = args.export_qasm_file
+    if s is not None:
+        q = qc.qasm()
+        _logger.debug("Exporing circuit")
+        with open(s, "w+") as f:
+            f.write(q)
+        print(q)
+        return
+    run(qc, selectors_q, args)
 
 
 if __name__ == "__main__":
