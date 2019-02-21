c43a3c6a6e0fe12cee80a047884860f245dddee4
Simone Perriello <simone.perriello@protonmail.com>
2019-02-21 11:50:10 +0100

    Added test cases and experiments for lee circuit

--- experiments/lee_ex.py
+++ experiments/lee_ex.py
@@ -1,9 +1,38 @@
 import numpy as np
-from isdquantum.methods.lee_brickell import LeeBrickellISD
+from isdquantum.methods.circuits.lee_brickell_bruteforce_circ import LeeBrickellCircuit
+from isdclassic.utils import rectangular_codes_hardcoded as rch
+from isdclassic.methods.lee_brickell import LeeBrickell
 import itertools
 
 
-def get_fixed_3():
+def get_fixed_1():
+    w = 2
+    p = 1
+    perm = np.array([
+        [0, 0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0],
+    ])
+    hr = np.array([
+        [0, 0, 1, 0, 1, 0, 0, 0],
+        [1, 0, 1, 1, 0, 1, 0, 0],
+        [1, 1, 0, 0, 0, 0, 1, 0],
+        [0, 1, 1, 1, 0, 0, 0, 1],
+    ])
+    v = hr[:, 0:4]
+    s_sig = np.array([1, 1, 0, 1])
+    exp_combination = (3)
+    exp_e_hat = [0, 0, 0, 1, 1, 0, 0, 0]
+    exp_e = np.array([1, 0, 0, 0, 0, 0, 1, 0])
+    return hr, v, perm, w, p, s_sig, exp_combination, exp_e_hat, exp_e
+
+
+def get_fixed_2():
     w = 2
     p = 1
     perm = np.array([
@@ -32,15 +61,98 @@ def get_fixed_3():
     return hr, v, perm, w, p, s_sig, exp_combination, exp_e_hat, exp_e
 
 
+def get_fixed_3():
+    w = 2
+    p = 2
+    perm = np.array([
+        [0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 0],
+    ])
+    hr = np.array([
+        [0, 1, 0, 0, 1, 0, 0, 0],
+        [1, 1, 0, 1, 0, 1, 0, 0],
+        [0, 1, 1, 1, 0, 0, 1, 0],
+        [1, 1, 1, 1, 0, 0, 0, 1],
+    ])
+    v = hr[:, 0:4]
+    s_sig = np.array([1, 0, 0, 0])
+    exp_combination = [1, 3]
+    exp_e_hat = np.array([0, 1, 0, 1, 0, 0, 0, 0])
+    exp_e = np.array([1, 0, 0, 0, 0, 0, 1, 0])
+    return hr, v, perm, w, p, s_sig, exp_combination, exp_e_hat, exp_e
+
+
+def get_fixed_4():
+    w = 2
+    p = 1
+    hr = np.array([
+        [0, 0, 1, 0, 1, 0, 0, 0],
+        [0, 1, 0, 1, 0, 1, 0, 0],
+        [1, 1, 1, 1, 0, 0, 1, 0],
+        [1, 0, 1, 1, 0, 0, 0, 1],
+    ])
+    v = hr[:, 0:4]
+    perm = np.array([
+        [0, 0, 0, 0, 1, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1],
+    ])
+    s_sig = np.array([1, 1, 1, 0])
+    exp_combination = [1]
+    exp_e_hat = np.array([0, 1, 0, 0, 1, 0, 0, 0])
+    exp_e = np.array([1, 1, 0, 0, 0, 0, 0, 0])
+
+    return hr, v, perm, w, p, s_sig, exp_combination, exp_e_hat, exp_e
+
+
+def get_fixed_5():
+    w = 2
+    p = 1
+    perm = np.array([
+        [0, 0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 1, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 1, 0],
+    ])
+    hr = np.array([
+        [1, 0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 1, 1, 0, 1, 0, 0],
+        [1, 1, 1, 1, 0, 0, 1, 0],
+        [1, 1, 0, 1, 0, 0, 0, 1],
+    ])
+    v = hr[:, 0:4]
+    s_sig = np.array([1, 1, 1, 0])
+    exp_combination = [2]
+    exp_e_hat = np.array([0, 0, 1, 0, 1, 0, 0, 0])
+    exp_e = np.array([1, 1, 0, 0, 0, 0, 0, 0])
+
+    return hr, v, perm, w, p, s_sig, exp_combination, exp_e_hat, exp_e
+
+
 # Try to launch quantum algorithm with a specific V obtained from classical algorithm
 # for which we are sure that there exist a correct error
 def ex_fixed():
-    hr, v, perm, w, p, s_sig, exp_comb, exp_e_hat, exp_e = get_fixed_3()
+    hr, v, perm, w, p, s_sig, exp_comb, exp_e_hat, exp_e = get_fixed_5()
     k = v.shape[0]
     wanted_sum = w - p
     print("v =\n{}".format(v))
-    print("s_sig = ".format(s_sig))
-    lb = LeeBrickellISD(hr, v, s_sig, w, p, True, 'advanced', 'benes')
+    print("s_sig = {}".format(s_sig))
+    lb = LeeBrickellCircuit(hr, v, s_sig, w, p, True, 'advanced', 'benes')
     qc = lb.build_circuit()
     from qiskit import BasicAer, execute
     result = execute(qc, BasicAer.get_backend('qasm_simulator')).result()
@@ -56,6 +168,10 @@ def ex_fixed():
     ]
     print("Error positions {}".format(error_positions))
     print("Expected error positions {}".format(exp_comb))
+    # exit(0)
+    print("Drawing")
+    # from isdquantum.utils import misc
+    # misc.draw_circuit(qc, 'data/img/exp/lee_fixed_5')
     v_extr = v[:, error_positions]
     sum_to_s = (v_extr.sum(axis=1) + s_sig) % 2
     sum_to_s_w = np.sum(sum_to_s)
@@ -82,9 +198,29 @@ def ex_fixed():
     print("Expected error is {}".format(exp_e))
 
 
+def ex_w_classic():
+    n = 8
+    k = 4
+    d = 4
+    w = 2
+    p = 2
+    h, _, syndromes, errors, w, _ = rch.get_isd_systematic_parameters(
+        n, k, d, w)
+    s = syndromes[1]
+    lee = LeeBrickell(h, s, w, p)
+    e = lee.run()
+    for k, v in lee.result.items():
+        print(k, v)
+    print(e)
+    # print(
+    #         "Launching TEST w/ n = {0}, k = {1}, d = {2}, w = {3}, p = {4}".
+    #         format(n, k, d, w, p))
+    # print("h = \n{0}".format(h))
+
+
 def main():
-    # ex_fixed_v()
     ex_fixed()
+    # ex_w_classic()
 
 
 if __name__ == "__main__":
---
+++ test/test_lee_brickell_alg_circuit.py
@@ -0,0 +1,99 @@
+import logging
+from parameterized import parameterized
+from test.common_circuit import CircuitTestCase
+from test.common_circuit import BasicTestCase
+from isdquantum.methods.algorithms.lee_brickell_mixed_alg import LeeBrickellMixedAlg
+from isdquantum.methods.circuits.lee_brickell_bruteforce_circ import LeeBrickellCircuit
+from isdclassic.methods.lee_brickell import LeeBrickell
+from isdclassic.utils import rectangular_codes_hardcoded as rch
+import numpy as np
+
+
+class LeeBrickellAlgTest(CircuitTestCase):
+    @classmethod
+    def setUpClass(cls):
+        CircuitTestCase.setUpClass()
+        other_logger = logging.getLogger('isdclassic')
+        other_logger.setLevel(cls.logger.level)
+        other_logger.handlers = cls.logger.handlers
+
+    @parameterized.expand([
+        ("n8_k4_d4_w2_p1", 8, 4, 4, 2, 1),
+    ])
+    # The idea is to first run the classical computation and
+    # from it obtain the V matrix. Then launch the quantum algorithm
+    # with this V matrix. In this way, we should run the QuantumCircuit
+    # just one time because it is sure that we can recover the error from this
+    # specific V matrix.
+    def test_fixed_v(self, name, n, k, d, w, p):
+        h, _, syndromes, errors, w, _ = rch.get_isd_systematic_parameters(
+            n, k, d, w)
+        self.logger.info(
+            "Launching TEST w/ n = {0}, k = {1}, d = {2}, w = {3}, p = {4}".
+            format(n, k, d, w, p))
+        self.logger.debug("h = \n{0}".format(h))
+        for i, s in enumerate(syndromes):
+            with self.subTest(s=s):
+                self.logger.info("Launching SUBTEST w/ s = {0}".format(s))
+                lee = LeeBrickell(h, s, w, p)
+                exp_e = lee.run()
+                self.logger.debug(
+                    "For s = {0}, w = {1}, p = {2} h = \n{3}\nerror is {4}".
+                    format(s, w, p, h, exp_e))
+                # Just a double check on the result of the classic algorithm
+                np.testing.assert_array_almost_equal(exp_e, errors[i])
+                self.logger.info(
+                    "LeeBrickell Classic ended, preparing quantum")
+                # for k, v in lee.result.items():
+                #     print("{} -> {}".format(k, v))
+                hr = lee.result['hr']
+                perm = lee.result['perm']
+                s_sig = lee.result['s_sig']
+                u = lee.result['u']
+                v = lee.result['v']
+                exp_e_hat = lee.result['e_hat']
+                exp_indexes = lee.result['indexes']
+                # print(v)
+                # print(s_sig)
+
+                # QUANTUM
+                wanted_sum = w - p
+                shots = 4098
+                # print("v =\n{}".format(v))
+                # print("s_sig = ".format(s_sig))
+                lb = LeeBrickellCircuit(hr, v, s_sig, w, p, True, 'advanced',
+                                        'benes')
+                qc = lb.build_circuit()
+                result = self.execute_qasm(qc, shots=shots)
+                counts = result.get_counts()
+                self.logger.info(counts)
+
+                # BUILD ERROR VECTOR
+                max_val = max(counts.values())
+                accuracy = max_val / shots
+                self.assertGreater(accuracy, 2 / 3)
+                max_val_status = max(counts, key=lambda key: counts[key])
+                error_positions = [
+                    pos for pos, char in enumerate(max_val_status[::-1])
+                    if char == '1'
+                ]
+                self.assertEqual(error_positions, list(exp_indexes))
+                # self.logger.debug("Error positions {}".format(error_positions))
+                # self.logger.debug("Expected error positions {}".format(indexes))
+                v_extr = v[:, error_positions]
+                sum_to_s = (v_extr.sum(axis=1) + s_sig) % 2
+                sum_to_s_w = np.sum(sum_to_s)
+                self.assertEqual(sum_to_s_w, wanted_sum)
+                e_hat = np.concatenate((np.zeros(k), sum_to_s))
+                for j in error_positions:
+                    e_hat[j] = 1
+                self.logger.debug(
+                    "e_hat after error position is {}".format(e_hat))
+                np.testing.assert_array_equal(e_hat, exp_e_hat)
+                # print("expected e_hat is {}".format(exp_e_hat))
+                e_hat_w = np.sum(e_hat)
+                self.assertEqual(e_hat_w, w)
+                e = np.mod(np.dot(e_hat, perm.T), 2)
+                np.testing.assert_array_equal(e, exp_e)
+                # print("Error is {}".format(e))
+                # print("Expected error is {}".format(exp_e))
